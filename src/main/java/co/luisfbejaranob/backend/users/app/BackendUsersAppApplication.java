a97015e258a89b624a66e29be7479e4e8c942bb7
luisfbejaranob <luisfbejaranob@outlook.com>
2024-08-24 07:33:25 -0500

    feat: 🌟 Initial project structure

---
+++ src/main/java/co/luisfbejaranob/backend/users/app/BackendUsersAppApplication.java
@@ -0,0 +1,13 @@
+package co.luisfbejaranob.backend.users.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUsersAppApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BackendUsersAppApplication.class, args);
+    }
+}
---
+++ src/test/java/co/luisfbejaranob/backend/users/app/BackendUsersAppApplicationTests.java
@@ -0,0 +1,12 @@
+package co.luisfbejaranob.backend.users.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendUsersAppApplicationTests
+{
+    @Test
+    void contextLoads()
+    {}
+}
